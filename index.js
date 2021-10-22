60587e729fa83af963a85886d3c7f108c3ed48c9
ash-development <ash@schnoor.email>
2021-10-22 13:38:05 +0000

    fixed spelling & added FS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
-const config = require("./config.json");
+const config = require("config.json");
 client.config = config;
 client.commands = new Discord.Collection();
 
